f41ca04cfb7191721ee76d41db288c359706440a
Ramirez Fragoso Ramses de Jesus <2604ram@gmail.com>
2025-02-25 23:28:44 -0600

    Add files via upload

---
+++ menú.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+	int main() {
+    struct Contacto contactos[100];
+    int num_contactos = 0;
+
+    int opcion;
+    do {
+        printf("\n--- Gestor de Contactos ---\n");
+        printf("1. Agregar Contacto\n");
+        printf("2. Mostrar Contactos\n");
+        printf("3. Buscar Contacto\n");
+        printf("4. Eliminar Contacto\n");
+        printf("5. Guardar Contactos\n");
+        printf("6. Cargar Contactos\n");
+        printf("0. Salir\n");
+        printf("porfavor selecciona una opcion y da enter: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1: agregarContacto(contactos, &num_contactos); break;
+            case 2: mostrarContactos(contactos, num_contactos); break;
+            case 3: buscarContacto(contactos, num_contactos); break;
+            case 4: eliminarContacto(contactos, &num_contactos); break;
+            case 5: guardarContactos(contactos, num_contactos); break;
+            case 6: cargarContactos(contactos, &num_contactos); break;
+            case 0: printf("\nsaliendo del programita...\n"); break;
+            default: printf("\nseleccina una opcion valida.\n");
+        }
+    } while (opcion != 0);
+    return 0;
+}
