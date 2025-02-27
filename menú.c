d182aa6f223b230b731059c7dff7b438516aa2e9
Ramirez Fragoso Ramses de Jesus <2604ram@gmail.com>
2025-02-27 01:08:42 -0600

    Add files via upload

--- menú.c
+++ menú.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
 int main() {
+    struct Contacto contactos[100];
+    int num_contactos = 0;
+	cargarContactos(contactos, &num_contactos);
     int opcion;
     do {
         printf("\n--- Gestor de Contactos ---\n");
@@ -11,20 +13,19 @@ int main() {
         printf("5. Guardar Contactos\n");
         printf("6. Cargar Contactos\n");
         printf("0. Salir\n");
-        printf("Por favor selecciona una opcion y da enter: ");
+        printf("porfavor selecciona una opcion y da enter: ");
         scanf("%d", &opcion);
 
         switch (opcion) {
-            case 1: printf("\nFuncion Agregar Contacto (no implementada)\n"); break;
-            case 2: printf("\nFuncion Mostrar Contactos (no implementada)\n"); break;
-            case 3: printf("\nFuncion Buscar Contacto (no implementada)\n"); break;
-            case 4: printf("\nFuncion Eliminar Contacto (no implementada)\n"); break;
-            case 5: printf("\nFuncion Guardar Contactos (no implementada)\n"); break;
-            case 6: printf("\nFuncion Cargar Contactos (no implementada)\n"); break;
-            case 0: printf("\nSaliendo del programita...\n"); break;
-            default: printf("\nSelecciona una opcion valida.\n");
+            case 1: agregarContacto(contactos, &num_contactos); break;
+            case 2: mostrarContactos(contactos, num_contactos); break;
+            case 3: buscarContacto(contactos, num_contactos); break;
+            case 4: eliminarContacto(contactos, &num_contactos); break;
+            case 5: guardarContactos(contactos, num_contactos); break;
+            case 6: cargarContactos(contactos, &num_contactos); break;
+            case 0: printf("\nsaliendo del programita...\n"); break;
+            default: printf("\nseleccina una opcion valida.\n");
         }
     } while (opcion != 0);
     return 0;
 }
-
